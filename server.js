3fb55f51c47c2b0d67040531101166061dceaeca
Prince Soni <1989prince.soni@gmail.com>
2023-10-07 15:28:24 -0500

    Create server.js
    Added server js file

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+var app = express();
+app.use(express.static('dist/gdms'));
+app.get('/', function (req, res,next) {
+    res.redirect('/');
+});
+app.listen(8080)
