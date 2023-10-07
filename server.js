fa60b22889939e1a85e362de62e868856fb45f8b
Kenny Dokania <kanny.dokania@gmail.com>
2023-10-07 16:54:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
+
 var app = express();
+app.use(cors())
 app.use(express.static('dist/gdms'));
 app.get('/', function (req, res,next) {
     res.redirect('/');
