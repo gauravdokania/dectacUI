f26cddd5c9d567e3760ffcf4a6af51065561cbd5
Mohan <singhmohan005@gmail.com>
2023-10-28 08:29:16 +0530

    missing file

---
+++ src/app/_models/userDetails.ts
@@ -0,0 +1,19 @@
+export class UserDetails {
+    client_id?: string;
+    serial_number?: string;
+    racer_name?: string;
+    first_name?: string;
+    middle_name?: string;
+    last_name?: string;
+    dob?: string;
+    address1?: string;
+    address2?: string;
+    city?: string;
+    state?: string;
+    zipcode?: number;
+    country?: string;
+    tel?: string;
+    tel2?: string;
+    email?: string;
+    profileimage?:string;
+}
